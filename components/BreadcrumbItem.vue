c20119b6b552b9162046cbe632d618d0ae9d2319
AdityaDwiNugroho <veldora5000@gmail.com>
2025-03-16 08:29:11 +0700

    feat: add BreadcrumbItem component

---
+++ components/BreadcrumbItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  to?: string
+  disabled?: boolean
+  isLast?: boolean
+}>()
+</script>
+
+<template>
+  <component
+    :is="to && !disabled ? 'router-link' : 'span'"
+    :to="to"
+    :class="[
+      'breadcrumb-item',
+      { 'text-grey': !isLast, 'text-primary font-weight-medium': isLast }
+    ]"
+  >
+    {{ text }}
+  </component>
+</template>
